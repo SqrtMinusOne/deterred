17c5b0599c8a22241c999149476d27a145ee7f1b
SqrtMinusOne <thexcloud@gmail.com>
2025-07-12 18:22:07 +0300

    template: buttons, images, some docstrings

--- deterred-template.el
+++ deterred-template.el
@@ -26,7 +26,7 @@
 ;; TODO
 
 ;;; Code:
-(require 'widget)
+(require 'button)
 (require 'outline)
 
 (defconst deterred-template--alist-nest-symbol "->")
@@ -34,26 +34,35 @@
 (defconst deterred-template--elt-symbols (cons "[" "]"))
 (defconst deterred-template--list-separator ";")
 (defconst deterred-template--quote-escape-symbol "@@@")
+(defconst deterred-template--function-start "f--")
 
 (defun deterred-template--escape (string)
+  "Replace escaped quotes in STRING.
+
+This is necessary because `libxml-parse-html-region' misses them in
+attribute lists, e.g. <div value=\"\\\"item\\\"\" /> (which also
+breaks helpful, by the way, but not the built-in `describe-function'."
   (replace-regexp-in-string (rx "\\\"") deterred-template--quote-escape-symbol string))
 
 (defun deterred-template--unescape (string)
+  "Restore escaped quotes in STRING."
   (replace-regexp-in-string (regexp-quote deterred-template--quote-escape-symbol)
                             "\"" string))
 
 (defun deterred-template--process-value (value)
   "If VALUE is a number, return it.
 
-Otherwise, try to `read' it."
+Otherwise, try to `read' it.
+
+This is meant to be used for parsing s-expressions, e.g. in
+<div a=\"(+ 1 2)\" b=\"1\" /> \"a\" will be string but \"b\" will be
+number."
   (cond ((numberp value) value)
         ((listp value) (error "Can't process %s as value" value))
         (t (read (deterred-template--unescape value)))))
 
 (defun deterred-template--parse-accessor-expr (accessor)
-  "Parse ACCESSOR.
-
-Accessor is a string that traverses an object."
+  "Parse an ACCESSOR expression."
   (let ((regexp (rx (| (literal deterred-template--alist-nest-symbol)
                        (literal deterred-template--plist-nest-symbol)
                        (:
@@ -100,11 +109,31 @@ Accessor is a string that traverses an object."
     expr))
 
 (defun deterred-template--parse-accessor (accessor)
-  (if (string-match-p (rx bos "(" (* nonl) ")" eos) accessor)
-      (read accessor)
-    (deterred-template--parse-accessor-expr accessor)))
+  "Parse ACCESSOR into an elisp expression.
+
+The accessor is either a Lisp expression in parentheses, which is
+`read', or a drill-down string.
+
+A drill-down string a specifier-separated list of keys.  The available
+specifiers are as follows:
+- -> for `alist-get'.  E.g.:
+  ->a   for (alist-get a ...)
+  ->'a  for (alist-get 'a ...)
+  ->:a  for (alist-get :a ...)
+  ->\"a\" for (alist-get \"a\" ... nil nil #'equal)
+- . - for `plist-get', the same principle as for alists.
+- [<number>] for `seq-elt', which works with all sequences.
+
+E.g. a->b.c[0]."
+  (cond
+   ((string-match-p (rx bos "(" (* nonl) ")" eos) accessor)
+    (read accessor))
+   ((string-match-p (rx bos "/") accessor)
+    accessor)
+   (t (deterred-template--parse-accessor-expr accessor))))
 
 (defun deterred-template--parse-children (elem)
+  "Apply `deterred-template--parse-tree' to all children of ELEM."
   (let (child-res)
     (mapc (lambda (child)
             (setq child-res
@@ -113,6 +142,44 @@ Accessor is a string that traverses an object."
     child-res))
 
 (defun deterred-template--parse-tree (elem &optional res)
+  "Parse ELEM into a template syntax tree.
+
+ELEM is a `libxml-parse-html-region' node.  RES is the recursive
+parameter.
+
+The return value is a list of syntax tree nodes.  One node is either a
+string or list with the tree values:
+- node type (always a symbol, get with `car')
+- parameters (can be anything, get with `cadr')
+- children (other nodes, get with `caddr').
+Note that this is unlike the HTML tree, where children are appended
+after `cadr'.
+
+See `deterred-deftemplate' for docs on syntax.  The nodes aren't
+perfectly mapped to tags.
+
+The available node types are:
+- eval - just evaluate the expresion.  The cadr is the expression.
+  The expression has to return string because it will be concatenated
+  later.
+- var - print the value of the variable.  The cadr in an alist with
+  two keys:
+  - accessor (see `deterred-template--parse-accessor')
+  - convert (either a function to convert the value to string or nil)
+- let - bind the variable for nest.  The cadr is the alist of
+  variables to bind.
+- mapconcat.  The cadr is an alist with the following keys:
+  - accessor (iterate over this value)
+  - var (bind the value to this variable)
+  - separator (concat the results with this symbol)
+- propertize.  The cadr is the list of properties to bind.
+- func - call the function on the child nodes.  The cadr is the alist
+  with the following keys:
+  - func
+  - args - the argument list.  `:value' is substituted with the
+    evaluation of child nodes.
+- br - just linebreak, no parameters.
+- trim - trim the evaluation of child nodes."
   (cond
    ;; Simple text
    ((stringp elem) (push (deterred-template--unescape elem) res))
@@ -147,7 +214,7 @@ Accessor is a string that traverses an object."
              ,(deterred-template--parse-children elem))
           res))
    ;; <mapconcat iter="accessor" var="iter">...</mapcar>
-   ;; <mapconcat iter="accessor" var="iter" separator="\n" >...</mapcar>
+   ;; <mapconcat iter="accessor" var="iter" separator="\n" >...</mapconcat>
    ((eq (car elem) 'mapconcat)
     (let ((iter (alist-get 'iter (cadr elem))))
       (unless iter
@@ -169,6 +236,45 @@ Accessor is a string that traverses an object."
                                        v deterred-template--list-separator))))
             ,(deterred-template--parse-children elem))
           res))
+   ;; <img src=".../" />
+   ((eq (car elem) 'img)
+    (let ((src (alist-get 'src (cadr elem))))
+      (unless src
+        (error "<img> must have the src attribute"))
+      (push
+       `(img ((source . ,(deterred-template--parse-accessor src))
+              (params
+               . ,(mapcan (lambda (elem)
+                            (list (intern (format ":%s" (car elem)))
+                                  (deterred-template--process-value (cdr elem))))
+                          (seq-filter
+                           (lambda (elem)
+                             (not (member (car elem) '(src))))
+                           (cadr elem)))))
+             nil)
+       res)))
+   ;; <f--function-name>...</f--function-name>
+   ;; <f--function-name args="(:value 1 2 3)">...</f--function-name>
+   ((string-match-p (rx bos (literal deterred-template--function-start))
+                    (symbol-name (car elem)))
+    (push `(func
+            ((func . ,(intern (substring (symbol-name (car elem))
+                                         (length deterred-template--function-start))))
+             (args . ,(if (alist-get 'args (cadr elem))
+                          (deterred-template--process-value
+                           (alist-get 'args (cadr elem)))
+                        '(:value))))
+            ,(deterred-template--parse-children elem))
+          res))
+   ((eq (car elem) 'when)
+    (let ((cond (alist-get 'cond (cadr elem))))
+      (unless cond
+        (error "<when> must have the cond attribute"))
+      (push `(func
+              ((func . when)
+               (args . (,(deterred-template--process-value cond) :value)))
+              ,(deterred-template--parse-children elem))
+            res)))
    ;; <b>, <i>, <u>
    ((member (car elem) '(b i u))
     (push `(propertize
@@ -184,19 +290,31 @@ Accessor is a string that traverses an object."
    ;; <trim>...</trim>
    ((eq (car elem) 'trim)
     (push `(trim nil ,(deterred-template--parse-children elem)) res))
+   ;; <line>...</line>
+   ((eq (car elem) 'line)
+    (push `(func
+            ((func . deterred-utils-make-line)
+             (args . (:value)))
+            ,(deterred-template--parse-children elem))
+          res))
    ;; <button click="(expr)>...</button>"
    ((member (car elem) '(button))
     (let ((click (alist-get 'click (cadr elem))))
       (unless click
         (error "<button> must have the \"click\" attribute"))
       (push `(button
-              ((notify . ,(deterred-template--process-value click)))
+              ((callback . ,(deterred-template--process-value click)))
               ,(deterred-template--parse-children elem))
             res)))
    (t (error "Unknown tag: %s" (car elem))))
   res)
 
 (defun deterred-template--reverse-tree (tree)
+  "Reverse DETERRED syntax TREE.
+
+This is necessary because `deterred-template--parse-tree' returns it
+in the wrong order, and it's impossible to reverse it there because
+the same list is appened on different recursive levels."
   (nreverse
    (mapcar (lambda (elem)
              (when (and (not (stringp elem))
@@ -208,6 +326,10 @@ Accessor is a string that traverses an object."
            tree)))
 
 (defun deterred-template--parse (template)
+  "Parse the TEMPLATE string and return its syntax tree.
+
+See `deterred-template--parse-tree' for the description of the
+latter."
   (let (tree)
     (with-temp-buffer
       (insert (deterred-template--escape template))
@@ -216,6 +338,7 @@ Accessor is a string that traverses an object."
      (deterred-template--parse-tree tree))))
 
 (defun deterred-template--tree-to-commands (tree)
+  "Convert syntax TREE into an elisp expression."
   `(concat
     ,@(mapcar
        (lambda (elem)
@@ -243,15 +366,76 @@ Accessor is a string that traverses an object."
                                 `(',(car item) ,(cdr item)))
                               (cadr elem))))
              ('br "\n")
+             ('func
+              `(,(alist-get 'func (cadr elem))
+                ,@(mapcar (lambda (arg)
+                            (if (eq arg :value)
+                                (deterred-template--tree-to-commands (caddr elem))
+                              arg))
+                          (alist-get 'args (cadr elem)))))
              ('trim `(string-trim ,(deterred-template--tree-to-commands (caddr elem))))
-             ;; ('button
-             ;;  `(widget-create
-             ;;    'push-button
-             ;;    :notify (lambda ()
-             ;;              ,(alist-get 'notify (cadr elem)))
-             ;;    ,(deterred-template--tree-to-commands (caddr elem))))
+             ('img `(let ((img (create-image ,(alist-get 'source (cadr elem))
+                                             nil nil
+                                             ,@(alist-get 'params (cadr elem)))))
+                      (if (image-type-available-p (image-property img :type))
+                          (propertize "[IMG]" 'display img)
+                        "[IMG]")))
+             ('button
+              `(apply #'propertize
+                      ,(deterred-template--tree-to-commands (caddr elem))
+                      'face 'button
+                      (button--properties
+                       ,(alist-get 'callback (cadr elem))
+                       nil ,(alist-get 'help-echo (cadr elem)))))
              (_ (error "Unknown element in syntax tree: %s" (car elem))))))
        tree)))
+
+(defmacro deterred-deftemplate (name args doc-string-or-value &optional value)
+  "Define DETERRED string template function.
+
+The purpose of this is to avoid manual writing of long and unwieldy
+expressions of `concat', `format', etc. by using an HTML-esque string,
+which is converted to such an expression by a macro.
+
+NAME is the resulting function is name, ARGS is its arguments.  The
+third argument, DOC-STRING-OR-VALUE is treated as docstring is VALUE
+is non-nil.
+
+The value is a string with the following tags:
+- <eval>(expr)</eval> - evaluate expr, which must always return string.
+- <var value=\"accessor\" /> - print the value accessed by the
+  accessor expression (see `deterred-template--parse-accessor' on
+  that).
+  If the value isn't string, the convert parameter required, which has
+  to be a function converting the value to string (`prin1-to-string'
+  is a universal one).
+- <let var1=\"accessor1\" var2=\"accessor2\">...</let> - bind the
+  variables for the child nodes.
+- <mapconcat iter=\"accessor\" var=\"iter\" separator=\"\n\">
+  ...</mapconcat> - iterate over iter, binding it to the variable var,
+  and concat the results with separator.  Only iter is required.
+- <propertize key1=\"value1; value2\" >...</propertize>
+- <f--function-name args=\"(:value)\">...</f--function-name> - call
+  function-name on children.
+  E.g. <f--string-pad args=\"(:value 20 nil t)\">...
+- <b>, <i>, <u> - make the children bold, italic, or underline.
+- <br> - insert \\n.
+- <trim> - trim children."
+
+  (declare (indent defun) (doc-string 3))
+  (let ((doc-string (when value doc-string-or-value))
+        (value (or value doc-string-or-value)))
+    `(defun ,name (,@args)
+       ,@(when doc-string `(,doc-string))
+       ,(deterred-template--tree-to-commands
+         (deterred-template--parse
+          (deterred-template--escape value))))))
+
+(defmacro deterred-inline-template (value)
+  (declare (indent 0))
+  (deterred-template--tree-to-commands
+   (deterred-template--parse
+    (deterred-template--escape value))))
 
 (defvar-local deterred-template-playground-template
     "A line of text
@@ -267,6 +451,9 @@ Accessor is a string that traverses an object."
 <trim><b><var value=\"iter\" convert=\"number-to-string\" /></b></trim>
 </mapconcat>
 </let>
+<br>
+<f--string-pad args=\"(:value 20 nil t)\">test</f--string-pad>
+<button click=\"(lambda (&rest _) (message \\\"Hello\\\"))\">kek</button>
 ")
 
 (defvar-local deterred-template-playground-html-tree nil)
